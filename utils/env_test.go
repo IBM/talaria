c72b73b3cd64f2cd732e58dd7149279abf2ec1ae
PawelK2012 <Kaim.paul@gmail.com>
2024-12-05 14:29:02 +0000

    Fixed unit test
    Signed-off-by: PawelK2012 <Kaim.paul@gmail.com>
    Signed-off-by: Vladimir Atanasov <vlado.atanasov@gmail.com>

--- utils/env_test.go
+++ utils/env_test.go
@@ -9,19 +9,25 @@ func TestGetEnvVar(t *testing.T) {
 	type args struct {
 		lookUpVar, defaultVal, envVarKeyToBeSet, envVarValToBeSet string
 	}
+
+	type Want struct {
+		envVar string
+		wasSet bool
+	}
 	tests := []struct {
 		name string
 		args args
-		want string
+		want Want
 	}{
-		{name: "Happy flow retriving env var", args: args{lookUpVar: "BROKER_HOST", defaultVal: "BROKER_HOST", envVarKeyToBeSet: "BROKER_HOST", envVarValToBeSet: "0.0.0.0"}, want: "0.0.0.0"},
-		{name: "Retriving env var returns default value", args: args{lookUpVar: "DUMMY", defaultVal: "0.0.0.0", envVarKeyToBeSet: "BROKER_HOST", envVarValToBeSet: ""}, want: "0.0.0.0"},
+		{name: "Happy flow retriving env var", args: args{lookUpVar: "BROKER_HOST", defaultVal: "BROKER_HOST", envVarKeyToBeSet: "BROKER_HOST", envVarValToBeSet: "0.0.0.0"}, want: Want{envVar: "0.0.0.0", wasSet: true}},
+		{name: "Retriving env var returns default value", args: args{lookUpVar: "DUMMY", defaultVal: "0.0.0.0", envVarKeyToBeSet: "BROKER_HOST", envVarValToBeSet: ""}, want: Want{envVar: "0.0.0.0", wasSet: false}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			os.Setenv(tt.args.envVarKeyToBeSet, tt.args.envVarValToBeSet)
-			if got := GetEnvVar(tt.args.lookUpVar, tt.args.defaultVal); got != tt.want {
-				t.Errorf("GetEnvVar() = %v, want %v", got, tt.want)
+			if got, wasSet := GetEnvVar(tt.args.lookUpVar, tt.args.defaultVal); got != tt.want.envVar || wasSet != tt.want.wasSet {
+				t.Errorf("GetEnvVar() = %v, want %v", got, tt.want.envVar)
+				t.Errorf("GetEnvVar() = %v, want %v", wasSet, tt.want.wasSet)
 			}
 		})
 	}
